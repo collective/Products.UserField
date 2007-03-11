5057d234f367b78e009402d4ebabe338acb2ffe8
yenzenz <yenzenz@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2007-03-11 16:32:58 +0000

    less verbose

--- utils.py
+++ utils.py
@@ -33,10 +33,10 @@ def takeRoleFromAllCurrentSet(obj, role, reindex=1, recursive=0):
     for uid in userids:
         currentroles = obj.get_local_roles_for_userid(userid=uid)
         filteredroles = [cur for cur in currentroles if cur != role]
-        print "delete local roles for %s" % uid ,
+        #print "delete local roles for %s" % uid ,
         obj.manage_delLocalRoles([uid])
         if filteredroles:                
-            print "but preserve roles %s" % filteredroles
+            #print "but preserve roles %s" % filteredroles
             obj.manage_setLocalRoles(uid, filteredroles)
     # if recursive: handle subobjects
     if recursive and hasattr( aq_base(obj), 'contentValues' ):
@@ -56,7 +56,7 @@ def setLocalRoles(instance, userids, roles=[], cumulative=False):
         for role in roles:
             takeRoleFromAllCurrentSet(instance, role, reindex=0)
     for uid in userids:
-        print ": add local roles %s for users %s" % (roles, uid)
+        #print ": add local roles %s for users %s" % (roles, uid)
         instance.manage_addLocalRoles(uid, roles)
         
     # It is assumed that all objects have the method
