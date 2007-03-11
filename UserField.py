47910723d75c0e992781abe81bda1920cca7aa0c
yenzenz <yenzenz@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2007-03-11 16:06:57 +0000

    major refactoring of local role handling

--- UserField.py
+++ UserField.py
@@ -52,6 +52,7 @@ from sets import Set
 import types
 from zope.component import ComponentLookupError
 from interfaces import IGenericGroupTranslation
+from utils import setLocalRoles
 ##/code-section module-header
 
 
@@ -86,21 +87,13 @@ class UserField(ObjectField):
     security.declarePrivate('getRaw')
     security.declarePrivate('set')
 
-    def getFullName(self, instance, casus=1):
-        userid = self.get(instance) or ''
-        mt = getToolByName(instance, 'portal_membership')
-        userinfo = mt.getMemberInfo(userid)
-        if userinfo is None or not userinfo['fullname']:
-            return userid
-        # BKS special
-        fullname = userinfo['fullname'].replace('_', ' ')
-        if casus == 1:
-            return fullname
-        else:
-            return 'Herrn/Frau %s' % fullname
+    def get(self, instance, **kwargs):
+        return ObjectField.get(self, instance, **kwargs)
+
 
     def getRaw(self, instance, **kwargs):
         return ObjectField.getRaw(self, instance, **kwargs)
+
 
     def set(self, instance, value, **kwargs):
         res = ObjectField.set(self, instance, value, **kwargs)
@@ -114,77 +107,27 @@ class UserField(ObjectField):
         else:
             raise ValueError, 'only strings and lists/tuples allowed, but you provided the value %s' % value
 
-        if self.localrole is not None:
+        if self.localrole:
             localrole = self.localrole
-            if type(localrole) in types.StringTypes:
+            if type(self.localrole) in types.StringTypes:
                 localrole = [localrole]
-            self.setLocalRoles(instance, users, localrole)
+            setLocalRoles(instance, users, localrole, cumulative=self.cumulative)
         return res
 
-    def get(self, instance, **kwargs):
-        return ObjectField.get(self, instance, **kwargs)
 
-    def addLocalRoles(self, instance, member_id,roles=[]):
-        pm = instance.portal_membership
-
-        for r in roles:
-              self._setLocalRoles(instance, member_ids=[member_id]
-                                    , member_role = r,
-                                    )
-    def _setLocalRoles(self, instance, member_ids, member_roles, reindex=True):
-        """Add local roles on an item.
-        """
-        if type(member_roles) in (type(''),type(u'')):
-            member_roles=[member_roles]
-
-        pm = instance.portal_membership
-
-        for member_id in member_ids:
-            roles = Set(instance.get_local_roles_for_userid( userid=member_id ))
-            roles.union_update(member_roles)
-            instance.manage_setLocalRoles( member_id, roles )
-
-        if reindex:
-            # It is assumed that all objects have the method
-            # reindexObjectSecurity, which is in CMFCatalogAware and
-            # thus PortalContent and PortalFolder.
-            instance.reindexObjectSecurity()
-
-    def setLocalRoles(self, instance, member_id, roles=[]):
-        pm = instance.portal_membership
-        if type(roles) in (types.ListType, types.TupleType):
-            roles = [roles]
-
-        for role in roles:
-            if not self.cumulative:
-                self.deleteLocalRolesByRole(instance, role)
-            self._setLocalRoles(instance, member_id, role)
-
-    def deleteLocalRolesByRole(self, obj, role, reindex=1, recursive=0):
-        """Delete local roles of specified members.
-        """
-
-        member_ids = obj.users_with_local_role(role)
-        # XXX
-        if 1 or _checkPermission(ChangeLocalRoles, obj):
-            for member_id in member_ids:
-                roles = [r for r in obj.get_local_roles_for_userid(userid=member_id)\
-                         if r != role]
-                print "delete local roles for %s" % member_id ,
-                obj.manage_delLocalRoles([member_id])
-                if roles:
-                    print "but preserve roles %s" % roles
-                    obj.manage_setLocalRoles(member_id, roles)
-        
-        # if recursive: handle subobjects
-        if recursive and hasattr( aq_base(obj), 'contentValues' ):
-            for subobj in obj.contentValues():
-                self.deleteLocalRolesByRole(subobj, member_ids, 0, 1)
-
-        if reindex:
-            # reindexObjectSecurity is always recursive
-            obj.reindexObjectSecurity()
-
+    def getFullName(self, instance, casus=1):
+        userid = self.get(instance) or ''
+        mt = getToolByName(instance, 'portal_membership')
+        userinfo = mt.getMemberInfo(userid)
+        if userinfo is None or not userinfo['fullname']:
+            return userid
+        # BKS special
+        fullname = userinfo['fullname'].replace('_', ' ')
+        if casus == 1:
+            return fullname
+        else:
+            return 'Herrn/Frau %s' % fullname
+                                                   
     def getDefault(self, instance):
         default = ObjectField.getDefault(self, instance)
         if default:
