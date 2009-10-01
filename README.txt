26ce6308d02e4412b187f6ddbf8f6f2a52601b4d
jensens <jensens@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2009-10-01 16:22:24 +0000

    added egg infrastructure - needs testing, save point for now, need to go home

---
+++ README.txt
@@ -0,0 +1,34 @@
+=========
+UserField
+=========
+
+-------
+Purpose
+-------
+
+UserField is an Archetypes field with some functionality to 
+
+* store user and/or group ids, 
+
+* set local roles based on the selection, 
+
+* group limitation. 
+
+and some more.
+
+--------------------
+Author and Copyright
+--------------------
+
+Authors: 
+
+- Phil Auersperg <phil@bluedynamics.com>,
+- Jens Klein <jens@bluedynamics.com>,  
+
+Copyright (C) 2006-2009 BlueDynamics Alliance, Austria
+
+This code was made for BKS Chur Project: 
+Communication-Platform/Intranet/e-learning.
+
+
+ 
---
+++ setup.py
@@ -0,0 +1,48 @@
+# Copyright 2008-2009, BlueDynamics Alliance, Austria - http://bluedynamics.com
+# BSD derivative License 
+
+from setuptools import setup, find_packages
+import sys, os
+
+version = '2.0'
+shortdesc = "Archetypes Field dealing with Plone Users and assign local roles."
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+license = open(os.path.join(os.path.dirname(__file__), 'LICENSE.txt')).read()
+
+setup(name='Products.UserField',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc + license,
+      classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Framework :: Zope2',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',            
+      ], 
+      keywords='zope zope2 plone archetypes field user',
+      author='BlueDynamics Alliance',
+      author_email='dev@bluedynamics.com',
+      url='https://svn.plone.org/svn/archetypes/MoreFieldsAndWidgets/UserField',
+      license='BSD',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['Products', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.Archetypes',
+          # Zope 2 dependencies are missing
+      ],
+      extras_require={
+          'test': [
+              'interlude',
+              # Zope 2 dependencies are missing
+          ]
+      },      
+)
+
